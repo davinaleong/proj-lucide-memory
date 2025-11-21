c97c3e1c7010b857a6c4694a68c1277141b8c368
Davina Leong <leong.shi.yun@gmail.com>
2025-11-21 15:44:57 +0800

    WIP capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.gracesoft',
+  appName: 'Lucide Memory',
+  webDir: 'dist'
+};
+
+export default config;
